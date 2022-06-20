8d969175e180d84fdb38557ffc7acf5cd9e00098
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-06-20 21:40:51 +0100

    update

--- myPersonal-Asset-Manager-app/App.tsx
+++ myPersonal-Asset-Manager-app/App.tsx
@@ -1,112 +1,55 @@
-/**
- * To finalize installation of react-native-gesture-handler,
- * add the following at the top (make sure it's at the top and there's nothing else before it) 
- * of your entry file, such as App.tsx:
- */
-import 'react-native-gesture-handler';
-
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
-// Before rendering any navigation stack, enableScreens for memory optimisation. See https://reactnavigation.org/docs/react-native-screens/
-import { enableScreens } from 'react-native-screens';
-enableScreens();
-
-/** 
- * Import createStackNavigator that we will use to create the stack navigator for the home page
- We shall see below how they are used
- */
-import { createStackNavigator } from '@react-navigation/stack';
-/** 
- * Import NavigationContainer that we will use to wrap the stack we will create.
-See in App component below how it is used
- */
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import App1 from './App1';
+import App2 from './App2';
 import { NavigationContainer } from '@react-navigation/native';
+//Below requires npm install react-native-vector-icons @types/react-native-vector-icons. Using it for Tab Navigator icons
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-/**
- * Import our components that we will create screens for.
- * Among them is a new Component named Home which will simply contain Button links to 
- * each of the other Component screens.
- * See Home.tsx for implementation for the said Home Component
- */
-import Home from './src/Home';
-import Component3 from './src/components/Component3';
-import Component7 from './src/components/Components7';
+const Tab = createBottomTabNavigator();
 
-//Create the Stack object
-const Stack = createStackNavigator();
-//if we want to pass initial parameters to the App Stack, we can first organize the typing as we did for HomeScreen in Home.tsx
-//We will then have to use initialParams in each Stack.Screen to indicate the params specified in types
-//e.g. for Component1Screen would be initialParams={{demoInitialParam:string}}
-/*
-type AppStackParamList = {
-  HomeScreen: undefined; //no parameters expected to be passed to route when called
-  Component1Screen: {demoInitialParam:string};
-  Component2Screen: {title:string} | undefined; //means that title may be optionally passed
-  Component4Screen: undefined;
-  Component5Screen: undefined;
-  Component6Screen: undefined;
-  Component7Screen: undefined;
-  Component8Screen: undefined;
-  Component9Screen: undefined;
-};
-const Stack = createStackNavigator<AppStackParamList>();
-*/
-
-
-//See https://reactnavigation.org/docs/stack-navigator/ for info on the prop elements for Stack.Navigator.
-
-//Prepare the App Stack with the Screens
-const AppStack = () => {
-    return (
-        <Stack.Navigator
-            initialRouteName='HomeScreen'
-         //   mode='card'
-          //  headerMode='screen'
-          //  keyboardHandlingEnabled={true}
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: 'darkblue',
-                    height: 140
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                    paddingTop: 60,
-                    paddingBottom: 10
-                },
-                headerRight: () => (
-                    <Image style={styles.logo}
-                        source={require('./src/img/PAU-Logo-Website.png')}
-                    />
-                ),
-                headerTitleAlign: 'left',
-                headerRightContainerStyle: {//there is also headerLeftContainerStyle if we want to use it
-                    paddingBottom: 33
-                }
-            }}
-        >
-            <Stack.Screen name="HomeScreen" component={Home} options={{ title: 'Home Screen' }} />
-            <Stack.Screen name="Component3Screen" component={Component3} options={{ title: 'Component 3' }} />
-            <Stack.Screen name="Component7Screen" component={Component7} options={{ title: 'Component 7' }} />
-        </Stack.Navigator>
-    )
-}
-
-//Pass the prepared AppStack to App wrapping it in NavigatorContainer
 const App: React.FC = () => {
+
+    //below are some optional props that can be passed to Tab.Navigator. You can try the code with and without options
+    const tabProps = {
+        initialRouteName: 'App1Screen',
+        tabBarOptions: {
+            activeTintColor: 'green',
+            inactiveTintColor: 'grey',
+            style: {
+                backgroundColor: '#eee',
+            },
+            backBehavior: 'history'//Behaviour when system back is touched. Options are none, initialRoute, order, history. This seems to be buggy
+        },
+        lazy: true //default is true
+    }
+
     return (
         <NavigationContainer>
-            <AppStack />
+            <Tab.Navigator {...tabProps}>
+                <Tab.Screen
+                    name="Personal Transactions Screen"
+                    component={App1}
+                    options={{
+                        tabBarLabel: 'Personal Transactions',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="home" color={color} size={size} />
+                        )
+                    }}
+                />
+                <Tab.Screen
+                    name="Personal Assets Screen"
+                    component={App2}
+                    options={{
+                        tabBarLabel: 'Personal Assets',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="bell" color={color} size={size} />
+                        )
+                    }}
+                />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
 
-const styles = StyleSheet.create({
-    logo: {
-        width: 133,
-        height: 55,
-        paddingBottom: 50
-    }
-});
-
-export default App;
+export default App
