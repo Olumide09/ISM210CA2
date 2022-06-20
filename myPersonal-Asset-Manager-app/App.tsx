f0e9686baf4aab2347c1f59fcbdbad723a39aa8a
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-06-20 16:09:55 +0100

    update

--- myPersonal-Asset-Manager-app/App.tsx
+++ myPersonal-Asset-Manager-app/App.tsx
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, KeyboardAvoidingView, ScrollView, Image, SafeAreaView, Platform } from 'react-native';
+import Component3 from './src/components/Component3';
+import Component4 from './src/components/Component3';
+import Component7 from './src/components/Component7';
+//Let's create a simple separator component that will only be used here in App.tsx
+const Separator: React.FC = () => {
+  return <View style={styles.separator}></View>
+}
 
-export default function App() {
+const App: React.FC = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={styles.container}>
+        <ScrollView>
+          <View style={styles.screenHeader}>
+            <Image style={styles.logo}
+              source={require('./src/img/PAU-Logo-Website.png')}
+            />
+          </View>
+          <View style={styles.screenBody}>
+            <Component3 />
+            <Separator />
+            <Component7 />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
+  /*
+  return(
+    <SafeAreaView style={[styles.container, {paddingTop: 30}]}>
+      <Component8 />
+    </SafeAreaView>
+  );
+  */
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'lightblue',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    padding: 15
+  },
+  separator: {
+    backgroundColor: '#eee',
+    height: 3,
+    width: '100%'
+  },
+  screenHeader: {
+    flex: 1,
+    paddingTop: 27,
+    justifyContent: 'center',
+    backgroundColor: 'darkblue'
+  },
+  screenBody: {
+    flex: 6,
     justifyContent: 'center',
   },
+  logo: {
+    alignSelf: 'center',
+    width: 200,
+    height: 82
+  }
 });
+
+export default App;
